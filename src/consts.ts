4e54cd4015b792485878d4eb7b36d1fd98df50b2
Anurag Pal <apal895@gmail.com>
2023-03-21 17:23:12 +0530

    qodeleaf block added

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Bubbble Studio Index";
+export const SITE_DESCRIPTION = "Bubbble Studio samples and index for content";
